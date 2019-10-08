d269639d32ce90f8cba675c3bc57c1d5de71cb48
Janaka-Steph <rochestephane@yahoo.fr>
2019-10-08 08:34:48 +0100

    Remove old comments

--- server/manager.ts
+++ server/manager.ts
@@ -1,16 +1,7 @@
 import EventEmitter from 'events';
-//import {ICoffee} from '../client/types';
 import {Invoice} from '@radar/lnrpc'
 
-// All logic and storage around posts happens in here. To keep things simple,
-// we're just storing posts in memory. Every time you restart the server, all
-// posts will be lost. For long term storage, you'd want to look into putting
-// these into a database.
-
-
 class Manager extends EventEmitter {
-  //coffees: ICoffee[] = [];
-
   constructor() {
     super()
   }
