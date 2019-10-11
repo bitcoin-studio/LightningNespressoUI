4d211a014b363e61046bfb2611d3a3137392dd3d
Janaka-Steph <rochestephane@yahoo.fr>
2019-10-11 22:12:32 +0100

    Fix deprecated `new Buffer()`

--- server/node.ts
+++ server/node.ts
@@ -6,7 +6,7 @@ export let node: LnRpc;
 export async function initNode() {
   node = await createLnRpc({
     server: env.LND_GRPC_URL,
-    cert: new Buffer(env.LND_TLS_CERT, 'base64').toString('ascii'),
-    macaroon: new Buffer(env.LND_MACAROON, 'base64').toString('hex'),
+    cert: Buffer.from(env.LND_TLS_CERT, 'base64').toString('ascii'),
+    macaroon: Buffer.from(env.LND_MACAROON, 'base64').toString('hex'),
   })
 }
